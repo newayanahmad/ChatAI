c83aa2fafd6104dda1d5178e5c4d909f951bfb45
Ayan Ahmad <newayanahmad@gmail.com>
2023-11-22 19:57:28 +0530

    Added clerk auth

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Button } from './ui/button'
+import { currentUser, UserButton } from "@clerk/nextjs"
+
+
+const Navbar = async () => {
+    const user = await currentUser()
+
+
+    console.log(user)
+    return (
+        <div className="w-full h-14 flex items-center justify-center shadow-md">
+            <div className='flex justify-around items-center w-full'>
+                <div className="left">Chat <span className='text-red-600'>AI</span></div>
+                <div className="right">
+                    <ul className='flex items-center justify-center gap-6'>
+                        <li className="cursor-pointer hover:text-blue-700">Pricing</li>
+                        <li className="cursor-pointer hover:text-blue-700">About</li>
+                        {user ? <UserButton /> : <><Button variant="outline" >SIGN IN</Button>
+                            <Button >Get Started</Button></>}
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
