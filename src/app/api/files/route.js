a8278e175901e9e1c9696416f7adbed604e881ab
Ayan Ahmad <newayanahmad@gmail.com>
2023-11-24 11:57:30 +0530

    Added OpenAI

--- src/app/api/files/route.js
+++ src/app/api/files/route.js
@@ -1,4 +1,4 @@
-import { FileModel } from '@/db/Schema'
+import { FileModel, User } from '@/db/Schema'
 import { NextResponse } from 'next/server'
 import { currentUser } from "@clerk/nextjs"
 import connectToDatabase from '@/db/connect'
@@ -7,9 +7,20 @@ export const POST = async (req) => {
     const { userId } = req.json()
     const user = await currentUser()
     await connectToDatabase()
-
+    console.log("user", user)
+    if (!user) return NextResponse.redirect(new URL("/sign-in", req.url))
+    const u = await User.findOne({ userId: user.id })
     if (!user) {
         return NextResponse.json({ error: 'Invalid user id' })
+    }
+    if (!u) {
+        const newUser = new User({
+            userId: user.id,
+            email: user.emailAddresses[0].emailAddress,
+            firstName: user.firstName,
+            lastName: user.lastName,
+        })
+        await newUser.save()
     }
     const files = await FileModel.find({ userId: user.id })
     return NextResponse.json({ success: true, files: files })
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = async () => {
     return (
         <div className="w-full absolute bg-white top-0 left-0 h-14 flex items-center justify-center shadow-md">
             <div className='flex justify-around items-center w-full'>
-                <Link href={"/"} className="left"><Image src={"/logo.jpg"} width={90} height={45} alt='ChatAI Logo' /></Link>
+                <Link href={"/"} className="left"><Image src={"/logo.png"} width={90} height={45} alt='ChatAI Logo' /></Link>
                 <div className="right">
                     <ul className='flex items-center justify-center gap-6'>
                         <li className="cursor-pointer hover:text-blue-700">Pricing</li>
