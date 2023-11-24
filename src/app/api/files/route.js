9711bc0daf5260abf56e6563aca0a1cbb1eb345f
Ayan Ahmad <newayanahmad@gmail.com>
2023-11-24 10:48:43 +0530

    Added OpenAI

--- src/app/api/files/route.js
+++ src/app/api/files/route.js
@@ -1,10 +1,12 @@
 import { FileModel } from '@/db/Schema'
 import { NextResponse } from 'next/server'
 import { currentUser } from "@clerk/nextjs"
+import connectToDatabase from '@/db/connect'
 
 export const POST = async (req) => {
     const { userId } = req.json()
     const user = await currentUser()
+    await connectToDatabase()
 
     if (!user) {
         return NextResponse.json({ error: 'Invalid user id' })
